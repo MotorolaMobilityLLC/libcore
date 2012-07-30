CyanogenMod/android_libcore

commit 26841c0bb838d54fea2e1a06390e413fab561e27

Remove useless checkIndex calls from the asXBuffer adapter classes.
Also add tests that we throw the expected exceptions.

Bug: 6085292
Change-Id: Ibcc44bdb546cba5365cdf557847e068fea77e1ca

luni/src/main/java/java/nio/CharToByteBufferAdapter.java
@@ -93,7 +93,6 @@ final class CharToByteBufferAdapter extends CharBuffer {
 
     @Override
     public char get(int index) {
-        checkIndex(index);
         return byteBuffer.getChar(index * SizeOf.CHAR);
     }
 

luni/src/main/java/java/nio/DoubleToByteBufferAdapter.java
@@ -93,7 +93,6 @@ final class DoubleToByteBufferAdapter extends DoubleBuffer {
 
     @Override
     public double get(int index) {
-        checkIndex(index);
         return byteBuffer.getDouble(index * SizeOf.DOUBLE);
     }
 

luni/src/main/java/java/nio/FloatToByteBufferAdapter.java
@@ -92,7 +92,6 @@ final class FloatToByteBufferAdapter extends FloatBuffer {
 
     @Override
     public float get(int index) {
-        checkIndex(index);
         return byteBuffer.getFloat(index * SizeOf.FLOAT);
     }
 

luni/src/main/java/java/nio/IntToByteBufferAdapter.java
@@ -93,7 +93,6 @@ final class IntToByteBufferAdapter extends IntBuffer {
 
     @Override
     public int get(int index) {
-        checkIndex(index);
         return byteBuffer.getInt(index * SizeOf.INT);
     }
 

luni/src/main/java/java/nio/LongToByteBufferAdapter.java
@@ -93,7 +93,6 @@ final class LongToByteBufferAdapter extends LongBuffer {
 
     @Override
     public long get(int index) {
-        checkIndex(index);
         return byteBuffer.getLong(index * SizeOf.LONG);
     }
 

luni/src/main/java/java/nio/ShortToByteBufferAdapter.java
@@ -92,7 +92,6 @@ final class ShortToByteBufferAdapter extends ShortBuffer {
 
     @Override
     public short get(int index) {
-        checkIndex(index);
         return byteBuffer.getShort(index * SizeOf.SHORT);
     }
 

luni/src/test/java/libcore/java/nio/BufferTest.java
@@ -675,4 +675,74 @@ public class BufferTest extends TestCase {
         }
         assertFalse(bb.hasArray());
     }
+
+    public void testBug6085292() {
+        ByteBuffer b = ByteBuffer.allocateDirect(1);
+
+        try {
+            b.asCharBuffer().get();
+            fail();
+        } catch (BufferUnderflowException expected) {
+        }
+        try {
+            b.asCharBuffer().get(0);
+            fail();
+        } catch (IndexOutOfBoundsException expected) {
+        }
+
+        try {
+            b.asDoubleBuffer().get();
+            fail();
+        } catch (BufferUnderflowException expected) {
+        }
+        try {
+            b.asDoubleBuffer().get(0);
+            fail();
+        } catch (IndexOutOfBoundsException expected) {
+        }
+
+        try {
+            b.asFloatBuffer().get();
+            fail();
+        } catch (BufferUnderflowException expected) {
+        }
+        try {
+            b.asFloatBuffer().get(0);
+            fail();
+        } catch (IndexOutOfBoundsException expected) {
+        }
+
+        try {
+            b.asIntBuffer().get();
+            fail();
+        } catch (BufferUnderflowException expected) {
+        }
+        try {
+            b.asIntBuffer().get(0);
+            fail();
+        } catch (IndexOutOfBoundsException expected) {
+        }
+
+        try {
+            b.asLongBuffer().get();
+            fail();
+        } catch (BufferUnderflowException expected) {
+        }
+        try {
+            b.asLongBuffer().get(0);
+            fail();
+        } catch (IndexOutOfBoundsException expected) {
+        }
+
+        try {
+            b.asShortBuffer().get();
+            fail();
+        } catch (BufferUnderflowException expected) {
+        }
+        try {
+            b.asShortBuffer().get(0);
+            fail();
+        } catch (IndexOutOfBoundsException expected) {
+        }
+    }
 }

MotorolaMobilityLLC/libcore

commit 4904cd1e3c21c4e18d441d551114393ea5a8a5c3

Repair the damage I did removing "useless" checkIndex calls from the asXBuffer adapter classes.
The checkIndex calls were unnecessary as far as throwing the right Exception
subclass goes, but they're necessary if you want the detail message to use
the units of the wrapping buffer rather than the wrapped buffer. And we want
that. An IndexOutOfBoundsException that says "offset=0, limit=1" is only
going to confuse developers.

(Fixes bug introduced by 26841c0bb838d54fea2e1a06390e413fab561e27.)

Bug: 6085292
Change-Id: I9a1a5991ad383b7712642e40bced9af91440bed2

luni/src/main/java/java/nio/CharToByteBufferAdapter.java
@@ -93,6 +93,7 @@ final class CharToByteBufferAdapter extends CharBuffer {
 
     @Override
     public char get(int index) {
+        checkIndex(index);
         return byteBuffer.getChar(index * SizeOf.CHAR);
     }
 

luni/src/main/java/java/nio/DoubleToByteBufferAdapter.java
@@ -93,6 +93,7 @@ final class DoubleToByteBufferAdapter extends DoubleBuffer {
 
     @Override
     public double get(int index) {
+        checkIndex(index);
         return byteBuffer.getDouble(index * SizeOf.DOUBLE);
     }
 

luni/src/main/java/java/nio/FloatToByteBufferAdapter.java
@@ -92,6 +92,7 @@ final class FloatToByteBufferAdapter extends FloatBuffer {
 
     @Override
     public float get(int index) {
+        checkIndex(index);
         return byteBuffer.getFloat(index * SizeOf.FLOAT);
     }
 

luni/src/main/java/java/nio/IntToByteBufferAdapter.java
@@ -93,6 +93,7 @@ final class IntToByteBufferAdapter extends IntBuffer {
 
     @Override
     public int get(int index) {
+        checkIndex(index);
         return byteBuffer.getInt(index * SizeOf.INT);
     }
 

luni/src/main/java/java/nio/LongToByteBufferAdapter.java
@@ -93,6 +93,7 @@ final class LongToByteBufferAdapter extends LongBuffer {
 
     @Override
     public long get(int index) {
+        checkIndex(index);
         return byteBuffer.getLong(index * SizeOf.LONG);
     }
 

luni/src/main/java/java/nio/ShortToByteBufferAdapter.java
@@ -92,6 +92,7 @@ final class ShortToByteBufferAdapter extends ShortBuffer {
 
     @Override
     public short get(int index) {
+        checkIndex(index);
         return byteBuffer.getShort(index * SizeOf.SHORT);
     }
 

luni/src/test/java/libcore/java/nio/BufferTest.java
@@ -688,6 +688,7 @@ public class BufferTest extends TestCase {
             b.asCharBuffer().get(0);
             fail();
         } catch (IndexOutOfBoundsException expected) {
+            assertTrue(expected.getMessage().contains("limit=0"));
         }
 
         try {
@@ -699,6 +700,7 @@ public class BufferTest extends TestCase {
             b.asDoubleBuffer().get(0);
             fail();
         } catch (IndexOutOfBoundsException expected) {
+            assertTrue(expected.getMessage().contains("limit=0"));
         }
 
         try {
@@ -710,6 +712,7 @@ public class BufferTest extends TestCase {
             b.asFloatBuffer().get(0);
             fail();
         } catch (IndexOutOfBoundsException expected) {
+            assertTrue(expected.getMessage().contains("limit=0"));
         }
 
         try {
@@ -721,6 +724,7 @@ public class BufferTest extends TestCase {
             b.asIntBuffer().get(0);
             fail();
         } catch (IndexOutOfBoundsException expected) {
+            assertTrue(expected.getMessage().contains("limit=0"));
         }
 
         try {
@@ -732,6 +736,7 @@ public class BufferTest extends TestCase {
             b.asLongBuffer().get(0);
             fail();
         } catch (IndexOutOfBoundsException expected) {
+            assertTrue(expected.getMessage().contains("limit=0"));
         }
 
         try {
@@ -743,6 +748,7 @@ public class BufferTest extends TestCase {
             b.asShortBuffer().get(0);
             fail();
         } catch (IndexOutOfBoundsException expected) {
+            assertTrue(expected.getMessage().contains("limit=0"));
         }
     }
 }