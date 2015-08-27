CyanogenMod/android_libcore

commit 0424d12f9b8a19c4a451282bc8ae974f6109424d

Get stricter about DOCTYPEs.
Allow them only before we've started parsing any
XML elements.

bug: 3452274
Change-Id: I7b146e4dae8c7aa23a0a227538ec02604f828400

xml/src/main/java/org/kxml2/io/KXmlParser.java
@@ -163,6 +163,9 @@ public class KXmlParser implements XmlPullParser, Closeable {
     private boolean degenerated;
     private int attributeCount;
 
+    // true iff. we've encountered the START_TAG of an XML element at depth == 0;
+    private boolean parsedTopLevelStartTag;
+
     /*
      * The current element's attributes arranged in groups of 4:
      * i + 0 = attribute namespace URI
@@ -416,6 +419,9 @@ public class KXmlParser implements XmlPullParser, Closeable {
                 break;
             case DOCDECL:
                 readDoctype(justOneToken);
+                if (parsedTopLevelStartTag) {
+                    throw new XmlPullParserException("Unexpected token", this, null);
+                }
                 break;
 
             default:
@@ -1128,6 +1134,9 @@ public class KXmlParser implements XmlPullParser, Closeable {
         }
 
         int sp = depth++ * 4;
+        if (depth == 1) {
+            parsedTopLevelStartTag = true;
+        }
         elementStack = ensureCapacity(elementStack, sp + 4);
         elementStack[sp + 3] = name;
 

MotorolaMobilityLLC/libcore

commit 64b721fa0732e7dd184fc68f9a82674d194bd293

Fix PullParser reuse regression
Commit 0424d12f9b8a19c4a451282bc8ae974f6109424d introduced
a regression: calling setInput() would not fully reset the
parser iff the parser had already parsed a <!DOCTYPE
tag.

This change contains a fix and a test.

Thanks to mike@ for the report.

Bug: https://code.google.com/p/android/issues/detail?id=182605
Change-Id: Icddc1c52bca3beaba7f5ee662d18757d6fce6cf2

xml/src/main/java/org/kxml2/io/KXmlParser.java
@@ -1606,6 +1606,7 @@ public class KXmlParser implements XmlPullParser, Closeable {
         this.reader = reader;
 
         type = START_DOCUMENT;
+        parsedTopLevelStartTag = false;
         name = null;
         namespace = null;
         degenerated = false;

luni/src/test/java/libcore/xml/PullParserTest.java
@@ -769,10 +769,24 @@ public abstract class PullParserTest extends TestCase {
     public void testWhitespacesAfterDOCTYPE() throws Exception {
         XmlPullParser parser = newPullParser();
         String test = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<!DOCTYPE root [\n" +
-            "<!ENTITY dummy \"dummy\">\n" +
-            "]>  \n" +
-            "<root></root>";
+                "<!DOCTYPE root [\n" +
+                "<!ENTITY dummy \"dummy\">\n" +
+                "]>  \n" +
+                "<root></root>";
+        assertParseSuccess(test, parser);
+    }
+
+    // Regression test for https://code.google.com/p/android/issues/detail?id=182605
+    public void testSetInputParserReuse() throws Exception {
+        XmlPullParser parser = newPullParser();
+        String test = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<!DOCTYPE root [\n" +
+                "<!ENTITY dummy \"dummy\">\n" +
+                "]>  \n" +
+                "<root></root>";
+        assertParseSuccess(test, parser);
+
+        // A second call to parser.setInput() on a parser should result in a fully-reset parser.
         assertParseSuccess(test, parser);
     }
 