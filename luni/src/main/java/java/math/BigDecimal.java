LineageOS/android_libcore

commit 52200aac041b0f019ecc8b91da5f34971c7b3112

Fix two different long overflow bugs in BigDecimal division.
The two bugs were in the line:
    longCompareTo(Math.abs(rem) * 2,Math.abs(divisor))

 - The first bug is that divisor can be Long.MIN_VALUE, abs() of
   which overflows back to Long.MIN_VALUE. This caused e.g. the
   following to evaluate to -1 when it should be 0:

   BigDecimal.ONE.divide(new BigDecimal(Long.MIN_VALUE, 0, HALF_EVEN)

 - The second bug is that Math.abs(rem) can be > 2^62, again
   leading to overflow. This caused the following to evaluate to
   0 when it should be 1:

   new BigDecimal(Long.MIN_VALUE / 2 - 1)
     .divide(new BigDecimal(Long.MIN_VALUE), 0, HALF_DOWN)

Bug: 28398726
Change-Id: I33427c005d2928b345c5d5fa2e04dbe3fb672371

luni/src/main/java/java/math/BigDecimal.java
@@ -1088,7 +1088,7 @@ public class BigDecimal extends Number implements Comparable<BigDecimal>, Serial
         if(scaledDivisor.bitLength() < 63) { // 63 in order to avoid out of long after *2
             long rem = remainder.longValue();
             long divisor = scaledDivisor.longValue();
-            compRem = longCompareTo(Math.abs(rem) * 2,Math.abs(divisor));
+            compRem = compareForRounding(rem, divisor);
             // To look if there is a carry
             compRem = roundingBehavior(quotient.testBit(0) ? 1 : 0,
                     sign * (5 + compRem), roundingMode);
@@ -1116,8 +1116,7 @@ public class BigDecimal extends Number implements Comparable<BigDecimal>, Serial
         int sign = Long.signum( scaledDividend ) * Long.signum( scaledDivisor );
         if (remainder != 0) {
             // Checking if:  remainder * 2 >= scaledDivisor
-            int compRem;                                      // 'compare to remainder'
-            compRem = longCompareTo(Math.abs(remainder) * 2,Math.abs(scaledDivisor));
+            int compRem = compareForRounding(remainder, scaledDivisor); // 'compare to remainder'
             // To look if there is a carry
             quotient += roundingBehavior(((int)quotient) & 1,
                     sign * (5 + compRem),
@@ -2703,9 +2702,36 @@ public class BigDecimal extends Number implements Comparable<BigDecimal>, Serial
         setUnscaledValue(integerAndFraction[0]);
     }
 
-    private static int longCompareTo(long value1, long value2) {
+    /**
+     * Returns -1, 0, and 1, respectively, if {@code value1 <, ==, > value2},
+     * respectively, when comparing without regard to the values' sign.
+     * This corresponds to the partial order where {@code 0L} is the smallest
+     * possible value and {@link Long#MIN_VALUE} is the largest because its
+     * absolute value is larger than any possible long value.
+     */
+    private static int compareAbsoluteValues(long value1, long value2) {
+        // Because Math.abs(Long.MIN_VALUE) == Long.MIN_VALUE,
+        // -1 <= Math.abs(v) - 1 <= Long.MAX_VALUE with correct ordering
+        // for v == Long.MIN_VALUE.
+        value1 = Math.abs(value1) - 1;
+        value2 = Math.abs(value2) - 1;
+        // Unlike Long.compare(), we guarantee to return specifically -1 and +1
         return value1 > value2 ? 1 : (value1 < value2 ? -1 : 0);
     }
+
+    /**
+     * Returns {@code -1, 0, +1} respectively for {@code abs(n/d) <, ==, > 0.5},
+     * respectively, using overflow and rounding safe arithmetics.
+     */
+    private static int compareForRounding(long n, long d) {
+        long halfD = d / 2;
+        if (n == halfD || n == -halfD) {
+            return (int) (d & 1);
+        } else {
+            return compareAbsoluteValues(n, halfD);
+        }
+    }
+
     /**
      * This method implements an efficient rounding for numbers which unscaled
      * value fits in the type {@code long}.
@@ -2727,7 +2753,7 @@ public class BigDecimal extends Number implements Comparable<BigDecimal>, Serial
         // If the discarded fraction is non-zero perform rounding
         if (fraction != 0) {
             // To check if the discarded fraction >= 0.5
-            compRem = longCompareTo(Math.abs(fraction) * 2, sizeOfFraction);
+            compRem = compareForRounding(fraction, sizeOfFraction);
             // To look if there is a carry
             integer += roundingBehavior( ((int)integer) & 1,
                     Long.signum(fraction) * (5 + compRem),

luni/src/test/java/libcore/java/math/BigDecimalTest.java
@@ -20,6 +20,8 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.util.Locale;
+
 import junit.framework.TestCase;
 
 public final class BigDecimalTest extends TestCase {
@@ -118,6 +120,34 @@ public final class BigDecimalTest extends TestCase {
 
         assertFalse(zero.equals(other));
         assertFalse(other.equals(zero));
+    }
+
+    private static void checkDivide(String expected, long n, long d, int scale, RoundingMode rm) {
+        assertEquals(String.format(Locale.US, "%d/%d [%d, %s]", n, d, scale, rm.name()),
+                new BigDecimal(expected),
+                new BigDecimal(n).divide(new BigDecimal(d), scale, rm));
+    }
+
+    public void testDivideRounding() {
+        checkDivide("0", 1, Long.MIN_VALUE, 0, RoundingMode.DOWN);
+        checkDivide("-1", 1, Long.MIN_VALUE, 0, RoundingMode.UP);
+        checkDivide("-1", 1, Long.MIN_VALUE, 0, RoundingMode.FLOOR);
+        checkDivide("0", 1, Long.MIN_VALUE, 0, RoundingMode.CEILING);
+        checkDivide("0", 1, Long.MIN_VALUE, 0, RoundingMode.HALF_EVEN);
+        checkDivide("0", 1, Long.MIN_VALUE, 0, RoundingMode.HALF_UP);
+        checkDivide("0", 1, Long.MIN_VALUE, 0, RoundingMode.HALF_DOWN);
+
+        checkDivide("1", Long.MAX_VALUE, Long.MAX_VALUE / 2 + 1, 0, RoundingMode.DOWN);
+        checkDivide("2", Long.MAX_VALUE, Long.MAX_VALUE / 2, 0, RoundingMode.DOWN);
+        checkDivide("0.50", Long.MAX_VALUE / 2, Long.MAX_VALUE, 2, RoundingMode.HALF_UP);
+        checkDivide("0.50", Long.MIN_VALUE / 2, Long.MIN_VALUE, 2, RoundingMode.HALF_UP);
+        checkDivide("0.5000", Long.MIN_VALUE / 2, Long.MIN_VALUE, 4, RoundingMode.HALF_UP);
+        // (-2^62 + 1) / (-2^63) = (2^62 - 1) / 2^63 = 0.5 - 2^-63
+        checkDivide("0", Long.MIN_VALUE / 2 + 1, Long.MIN_VALUE, 0, RoundingMode.HALF_UP);
+        checkDivide("1", Long.MIN_VALUE / 2, Long.MIN_VALUE, 0, RoundingMode.HALF_UP);
+        checkDivide("0", Long.MIN_VALUE / 2, Long.MIN_VALUE, 0, RoundingMode.HALF_DOWN);
+        // (-2^62 - 1) / (-2^63) = (2^62 + 1) / 2^63 = 0.5 + 2^-63
+        checkDivide("1", Long.MIN_VALUE / 2 - 1, Long.MIN_VALUE, 0, RoundingMode.HALF_DOWN);
     }
 
     /**

MotorolaMobilityLLC/libcore

commit 26223d4520e7f4756d359aaabe92efa392b7033a

Fix BigDecimal rounding 50/101 up rather than down.
That bug was introduced by 52200aac041b0f019ecc8b91da5f34971c7b3112;
it was a sign error in the case of denominator = numerator * 2 + 1.

This change fixes the bug and adds additional tests to avoid regression,
plus comments explaining why the code is now correct.

Bug: 28398726
Change-Id: Ic95726edd9895bb91665943cb0aa359c38dd2a98

luni/src/main/java/java/math/BigDecimal.java
@@ -2703,16 +2703,21 @@ public class BigDecimal extends Number implements Comparable<BigDecimal>, Serial
     }
 
     /**
-     * Returns -1, 0, and 1, respectively, if {@code value1 <, ==, > value2},
-     * respectively, when comparing without regard to the values' sign.
-     * This corresponds to the partial order where {@code 0L} is the smallest
-     * possible value and {@link Long#MIN_VALUE} is the largest because its
-     * absolute value is larger than any possible long value.
+     * Returns -1, 0, and 1 if {@code value1 < value2}, {@code value1 == value2},
+     * and {@code value1 > value2}, respectively, when comparing without regard
+     * to the values' sign.
+     *
+     * <p>Note that this implementation deals correctly with Long.MIN_VALUE,
+     * whose absolute magnitude is larger than any other {@code long} value.
      */
     private static int compareAbsoluteValues(long value1, long value2) {
-        // Because Math.abs(Long.MIN_VALUE) == Long.MIN_VALUE,
-        // -1 <= Math.abs(v) - 1 <= Long.MAX_VALUE with correct ordering
-        // for v == Long.MIN_VALUE.
+        // Map long values to the range -1 .. Long.MAX_VALUE so that comparison
+        // of absolute magnitude can be done using regular long arithmetics.
+        // This deals correctly with Long.MIN_VALUE, whose absolute magnitude
+        // is larger than any other long value, and which is mapped to
+        // Long.MAX_VALUE here.
+        // Values that only differ by sign get mapped to the same value, for
+        // example both +3 and -3 get mapped to +2.
         value1 = Math.abs(value1) - 1;
         value2 = Math.abs(value2) - 1;
         // Unlike Long.compare(), we guarantee to return specifically -1 and +1
@@ -2720,14 +2725,29 @@ public class BigDecimal extends Number implements Comparable<BigDecimal>, Serial
     }
 
     /**
-     * Returns {@code -1, 0, +1} respectively for {@code abs(n/d) <, ==, > 0.5},
-     * respectively, using overflow and rounding safe arithmetics.
+     * Compares {@code n} against {@code 0.5 * d} in absolute terms (ignoring sign)
+     * and with arithmetics that are safe against overflow or loss of precision.
+     * Returns -1 if {@code n} is less than {@code 0.5 * d}, 0 if {@code n == 0.5 * d},
+     * or +1 if {@code n > 0.5 * d} when comparing the absolute values under such
+     * arithmetics.
      */
     private static int compareForRounding(long n, long d) {
-        long halfD = d / 2;
+        long halfD = d / 2; //  rounds towards 0
         if (n == halfD || n == -halfD) {
-            return (int) (d & 1);
+            // In absolute terms: Because n == halfD, we know that 2 * n + lsb == d
+            // for some lsb value 0 or 1. This means that n == d/2 (result 0) if
+            // lsb is 0, or n < d/2 (result -1) if lsb is 1. In either case, the
+            // result is -lsb.
+            // Since we're calculating in absolute terms, we need the absolute lsb
+            // (d & 1) as opposed to the signed lsb (d % 2) which would be -1 for
+            // negative odd values of d.
+            int lsb = (int) d & 1;
+            return -lsb; // returns 0 or -1
         } else {
+            // In absolute terms, either 2 * n + 1 < d (in the case of n < halfD),
+            // or 2 * n > d (in the case of n > halfD).
+            // In either case, comparing n against halfD gets the right result
+            // -1 or +1, respectively.
             return compareAbsoluteValues(n, halfD);
         }
     }

luni/src/test/java/libcore/java/math/BigDecimalTest.java
@@ -129,6 +129,7 @@ public final class BigDecimalTest extends TestCase {
     }
 
     public void testDivideRounding() {
+        // checkDivide(expected, dividend, divisor, scale, roundingMode)
         checkDivide("0", 1, Long.MIN_VALUE, 0, RoundingMode.DOWN);
         checkDivide("-1", 1, Long.MIN_VALUE, 0, RoundingMode.UP);
         checkDivide("-1", 1, Long.MIN_VALUE, 0, RoundingMode.FLOOR);
@@ -148,6 +149,58 @@ public final class BigDecimalTest extends TestCase {
         checkDivide("0", Long.MIN_VALUE / 2, Long.MIN_VALUE, 0, RoundingMode.HALF_DOWN);
         // (-2^62 - 1) / (-2^63) = (2^62 + 1) / 2^63 = 0.5 + 2^-63
         checkDivide("1", Long.MIN_VALUE / 2 - 1, Long.MIN_VALUE, 0, RoundingMode.HALF_DOWN);
+    }
+
+    /**
+     * Test a bunch of pairings with even/odd dividend and divisor whose
+     * result is near +/- 0.5.
+     */
+    public void testDivideRounding_sign() {
+        // checkDivide(expected, dividend, divisor, scale, roundingMode)
+        // positive dividend and divisor, even/odd values
+        checkDivide("0", 49, 100, 0, RoundingMode.HALF_UP);
+        checkDivide("1", 50, 100, 0, RoundingMode.HALF_UP);
+        checkDivide("1", 51, 101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", 50, 101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", Long.MAX_VALUE / 2, Long.MAX_VALUE, 0, RoundingMode.HALF_UP);
+
+        // Same with negative dividend and divisor
+        checkDivide("0", -49, -100, 0, RoundingMode.HALF_UP);
+        checkDivide("1", -50, -100, 0, RoundingMode.HALF_UP);
+        checkDivide("1", -51, -101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", -50, -101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", -(Long.MAX_VALUE / 2), -Long.MAX_VALUE, 0, RoundingMode.HALF_UP);
+
+        // Same with negative dividend
+        checkDivide("0", -49, 100, 0, RoundingMode.HALF_UP);
+        checkDivide("-1", -50, 100, 0, RoundingMode.HALF_UP);
+        checkDivide("-1", -51, 101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", -50, 101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", -(Long.MAX_VALUE / 2), Long.MAX_VALUE, 0, RoundingMode.HALF_UP);
+
+        // Same with negative divisor
+        checkDivide("0", 49, -100, 0, RoundingMode.HALF_UP);
+        checkDivide("-1", 50, -100, 0, RoundingMode.HALF_UP);
+        checkDivide("-1", 51, -101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", 50, -101, 0, RoundingMode.HALF_UP);
+        checkDivide("0", Long.MAX_VALUE / 2, -Long.MAX_VALUE, 0, RoundingMode.HALF_UP);
+    }
+
+    public void testDivideByOne() {
+        long[] dividends = new long[] {
+                Long.MIN_VALUE,
+                Long.MIN_VALUE + 1,
+                Long.MAX_VALUE,
+                Long.MAX_VALUE - 1,
+                0,
+                -1,
+                1,
+                10, 43, 314159265358979323L, // arbitrary values
+        };
+        for (long dividend : dividends) {
+            String expected = Long.toString(dividend);
+            checkDivide(expected, dividend, 1, 0, RoundingMode.UNNECESSARY);
+        }
     }
 
     /**
@@ -176,7 +229,7 @@ public final class BigDecimalTest extends TestCase {
                 minLong.divide(new BigDecimal("-1E+1"), /* scale = */ 1, RoundingMode.UNNECESSARY));
 
         // cases that request adjustment of the result scale, i.e. (diffScale != 0)
-        // i.e. result scale != (numerator.scale - divisor.scale)
+        // i.e. result scale != (dividend.scale - divisor.scale)
         assertEquals("9223372036854775808/(-1) with one decimal of precision",//
                 new BigDecimal("9223372036854775808.0"),
                 minLong.divide(new BigDecimal("-1"), /* scale = */ 1, RoundingMode.UNNECESSARY));