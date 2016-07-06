LineageOS/android_libcore

commit 3683e911af7dde39dc7b4a8bbf4dc9e30e6e91ea

Make ChoiceFormat take defensive copies of its arrays
Findbugs reported some problems with ChoiceFormat and how it
handles its internal arrays. The upstream bug number is 8001209
but it's not visible in the externally visible database,
presumably because it's a security issue.

This adds tests to check that the internal arrays are not
visible externally in any way and the patch to ensure that those
tests pass.

Bug: 29352743
Testing: Added tests, ran tests, tests failed, applied fix,
tests passed.

Change-Id: I2595acb9bc8845c1a28bd8507a0a431cb970f8b9

ojluni/src/main/java/java/text/ChoiceFormat.java
@@ -345,8 +345,8 @@ public class ChoiceFormat extends NumberFormat {
             throw new IllegalArgumentException(
                 "Array and limit arrays must be of the same length.");
         }
-        choiceLimits = limits;
-        choiceFormats = formats;
+        choiceLimits = Arrays.copyOf(limits, limits.length);
+        choiceFormats = Arrays.copyOf(formats, formats.length);
     }
 
     /**
@@ -354,7 +354,8 @@ public class ChoiceFormat extends NumberFormat {
      * @return the limits.
      */
     public double[] getLimits() {
-        return choiceLimits;
+        double[] newLimits = Arrays.copyOf(choiceLimits, choiceLimits.length);
+        return newLimits;
     }
 
     /**
@@ -362,7 +363,8 @@ public class ChoiceFormat extends NumberFormat {
      * @return the formats.
      */
     public Object[] getFormats() {
-        return choiceFormats;
+        Object[] newFormats = Arrays.copyOf(choiceFormats, choiceFormats.length);
+        return newFormats;
     }
 
     // Overrides

luni/src/test/java/libcore/java/text/ChoiceFormatTest.java
@@ -1,0 +1,75 @@
+package libcore.java.text;
+
+import java.text.ChoiceFormat;
+import junit.framework.TestCase;
+
+/**
+ */
+public class ChoiceFormatTest extends TestCase {
+
+    /**
+     * Limits for {@link ChoiceFormat}, will be modified by some tests to ensure that ChoiceFormat
+     * stores a copy of the arrays provided.
+     */
+    private final double[] limits = new double[] { 0, 1, 2, 3, 4 };
+
+    /**
+     * Format strings for {@link ChoiceFormat}, will be modified by some tests to ensure that
+     * ChoiceFormat stores a copy of the arrays provided.
+     */
+    private final String[] formats = new String[] { "zero", "one", "a couple", "a few", "some" };
+
+    public void testConstructor_doubleArray_StringArray() throws Exception {
+        ChoiceFormat format = new ChoiceFormat(limits, formats);
+
+        verifyChoiceFormatCopiesSuppliedArrays(format);
+    }
+
+    public void testSetChoices() throws Exception {
+        ChoiceFormat format = new ChoiceFormat(new double[] { 0 }, new String[] { "" });
+        assertEquals("", format.format(1.4));
+
+        // Change the limits.
+        format.setChoices(limits, formats);
+
+        verifyChoiceFormatCopiesSuppliedArrays(format);
+    }
+
+    private void verifyChoiceFormatCopiesSuppliedArrays(ChoiceFormat format) {
+        assertEquals("one", format.format(1.4));
+
+        // Change the formats array and make sure that it doesn't affect the ChoiceFormat.
+        formats[1] = "uno";
+        assertEquals("ChoiceFormat doesn't make defensive copies of formats array",
+                "one", format.format(1.4));
+
+        // Change the limits array and make sure that it doesn't affect the ChoiceFormat.
+        limits[2] = 1.2;
+        assertEquals("ChoiceFormat doesn't make defensive copies of limits array",
+                "one", format.format(1.4));
+    }
+
+    public void testGetLimits() throws Exception {
+        ChoiceFormat format = new ChoiceFormat(limits, formats);
+        assertEquals("some", format.format(100));
+
+        // Get the limits array, change the contents and make sure it doesn't affect the behavior
+        // of the format.
+        double[] copiedLimits = format.getLimits();
+        copiedLimits[4] = 200;
+        assertEquals("ChoiceFormat doesn't return a copy of choiceLimits array",
+                "some", format.format(100));
+    }
+
+    public void testGetFormats() throws Exception {
+        ChoiceFormat format = new ChoiceFormat(limits, formats);
+        assertEquals("zero", format.format(-4));
+
+        // Get the formats array, change the contents and make sure it doesn't affect the behavior
+        // of the format.
+        Object[] copiedFormats = format.getFormats();
+        copiedFormats[0] = "none or less";
+        assertEquals("ChoiceFormat doesn't return a copy of choiceFormats array",
+                "zero", format.format(-4));
+    }
+}

MotorolaMobilityLLC/libcore

commit 2b128a25d4f35b94ec611a87366937a352e671e3

Removed tests that checked previous behavior of ChoiceFormat
Fixes test breakages caused by commit
3683e911af7dde39dc7b4a8bbf4dc9e30e6e91ea

Bug: 29352743
Change-Id: Ie2362044860a4850c057b6b8a0cfbc58d77112db
Test: Ran both ChoiceFormatTest classes.

ojluni/src/main/java/java/text/ChoiceFormat.java
@@ -351,6 +351,10 @@ public class ChoiceFormat extends NumberFormat {
 
     /**
      * Get the limits passed in the constructor.
+     *
+     * <p>The contents of the array are the same as the array passed into the constructor but the
+     * array will not be the same array.
+     *
      * @return the limits.
      */
     public double[] getLimits() {
@@ -360,6 +364,10 @@ public class ChoiceFormat extends NumberFormat {
 
     /**
      * Get the formats passed in the constructor.
+     *
+     * <p>The contents of the array are the same as the array passed into the constructor but the
+     * array will not be the same array.
+     *
      * @return the formats.
      */
     public Object[] getFormats() {

harmony-tests/src/test/java/org/apache/harmony/tests/java/text/ChoiceFormatTest.java
@@ -294,35 +294,6 @@ public class ChoiceFormatTest extends TestCase {
     }
 
     /**
-     * @tests java.text.ChoiceFormat#getFormats()
-     */
-    public void test_getFormats() {
-        // Test for method java.lang.Object []
-        // java.text.ChoiceFormat.getFormats()
-        String[] orgFormats = (String[]) formats.clone();
-        String[] f = (String[]) f1.getFormats();
-        // getFormats() documentation says "Get the formats passed in the constructor",
-        // which can be interpreted as object identity.
-        assertTrue("Wrong formats", f == formats);
-        f[0] = "Modified";
-        assertTrue("Formats copied", f != orgFormats);
-    }
-
-    /**
-     * @tests java.text.ChoiceFormat#getLimits()
-     */
-    public void test_getLimits() {
-        // Test for method double [] java.text.ChoiceFormat.getLimits()
-        double[] orgLimits = (double[]) limits.clone();
-        double[] l = f1.getLimits();
-        // getLimits() documentation says "Get the limits passed in the constructor",
-        // which can be interpreted as object identity.
-        assertTrue("Wrong limits", l == limits);
-        l[0] = 3.14527;
-        assertTrue("Limits copied", l != orgLimits);
-    }
-
-    /**
      * @tests java.text.ChoiceFormat#hashCode()
      */
     public void test_hashCode() {
@@ -392,20 +363,6 @@ public class ChoiceFormatTest extends TestCase {
         assertTrue("Not less -5", ChoiceFormat.previousDouble(-5) < -5);
         assertTrue("Not NaN", Double.isNaN(ChoiceFormat
                 .previousDouble(Double.NaN)));
-    }
-
-    /**
-     * @tests java.text.ChoiceFormat#setChoices(double[], java.lang.String[])
-     */
-    public void test_setChoices$D$Ljava_lang_String() {
-        // Test for method void java.text.ChoiceFormat.setChoices(double [],
-        // java.lang.String [])
-        ChoiceFormat f = (ChoiceFormat) f1.clone();
-        double[] l = new double[] { 0, 1 };
-        String[] fs = new String[] { "0", "1" };
-        f.setChoices(l, fs);
-        assertTrue("Limits copied", f.getLimits() == l);
-        assertTrue("Formats copied", f.getFormats() == fs);
     }
 
 	/**