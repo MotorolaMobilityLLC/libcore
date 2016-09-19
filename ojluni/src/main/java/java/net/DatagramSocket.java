LineageOS/android_libcore

commit 6764aa32a4b66a3c589f4a00615a1e36d87f308f

Fix NPE when sending DatagramPacket
This is due to code after

  getImpl().connect(address, port);

is not executed if it throws exception, leaving DatagramPacket in an
inconsistant state.

Re-arrange the order of code so that the state is now consistent.

Bug: 31218085
Test: libcore.java.net.DatagramSocketTest
Change-Id: Iab9bfd9af211666564e49de62fdae964d6330daf

ojluni/src/main/java/java/net/DatagramSocket.java
@@ -172,10 +172,11 @@ class DatagramSocket implements java.io.Closeable {
                 // connection will be emulated by DatagramSocket
                 connectState = ST_CONNECTED_NO_IMPL;
           }*/
-          getImpl().connect(address, port);
 
           // socket is now connected by the impl
           connectState = ST_CONNECTED;
+          getImpl().connect(address, port);
+
           // Do we need to filter some packets?
           int avail = getImpl().dataAvailable();
           if (avail == -1) {

luni/src/test/java/libcore/java/net/DatagramSocketTest.java
@@ -21,6 +21,7 @@ import junit.framework.TestCase;
 import java.lang.reflect.Field;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.DatagramSocketImpl;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
@@ -95,6 +96,24 @@ public class DatagramSocketTest extends TestCase {
     for (int i = 0; i <= 255; ++i) {
       s.setTrafficClass(i);
       assertEquals(i, s.getTrafficClass());
+    }
+  }
+
+  // Socket should become connected even if impl.connect() failed and threw exception.
+  public void test_b31218085() throws Exception {
+    final int port = 9999;
+
+    try (DatagramSocket s = new DatagramSocket()) {
+      // Set fd of DatagramSocket to null, forcing impl.connect() to throw.
+      Field f = DatagramSocket.class.getDeclaredField("impl");
+      f.setAccessible(true);
+      DatagramSocketImpl impl = (DatagramSocketImpl) f.get(s);
+      f = DatagramSocketImpl.class.getDeclaredField("fd");
+      f.setAccessible(true);
+      f.set(impl, null);
+
+      s.connect(InetAddress.getLocalHost(), port);
+      assertTrue(s.isConnected());
     }
   }
 }

MotorolaMobilityLLC/libcore

commit 31fa181866c5bd71582590532eb38b8b045f8fe6

Fix DatagramSocket.disconnect()
Fix DatagramSocket.disconnect() in the case where an exception has
been thrown during connect().

commit 6764aa32a4b66a3c589f4a00615a1e36d87f308f introduced a bug
because it would leave connectState as ST_CONNECTED when
connect() failed. The correct state in this case appears to be
ST_CONNECTED_NO_IMPL instead (which is then dealt with correctly
by disconnect()).

This change rearranges the Android changes over upstream to make it
closer to upstream and try to make it clearer what state is guaranteed
after an exception is thrown.

Test: Ran CtsLibcoreTestCases
Bug: 31218085
Change-Id: Ife3aba579b77fd05c64244c1aa5a6cb2ea7b304a

ojluni/src/main/java/java/net/DatagramSocket.java
@@ -152,47 +152,44 @@ class DatagramSocket implements java.io.Closeable {
         if (!isBound())
           bind(new InetSocketAddress(0));
 
-        // ----- BEGIN android -----
-        connectedAddress = address;
-        connectedPort = port;
-        // ----- END android -----
-
-        // old impls do not support connect/disconnect
-        if (oldImpl || (impl instanceof AbstractPlainDatagramSocketImpl &&
-             ((AbstractPlainDatagramSocketImpl)impl).nativeConnectDisabled())) {
-            connectState = ST_CONNECTED_NO_IMPL;
-        } else {
-          /* ----- BEGIN android -----
-          try {
-              getImpl().connect(address, port);
-
-              // socket is now connected by the impl
-              connectState = ST_CONNECTED;
-          } catch (SocketException se) {
-                // connection will be emulated by DatagramSocket
+        // Android-changed: This section now throws any SocketException generated by connect()
+        // to enable it to be recorded as the pendingConnectException. It has been enclosed in a
+        // try-finally to ensure connectedAddress and connectedPort are set when the exception
+        // is thrown.
+        try {
+            // old impls do not support connect/disconnect
+            // Android-changed: Added special handling for AbstractPlainDatagramSocketImpl in
+            // the condition below.
+            if (oldImpl || (impl instanceof AbstractPlainDatagramSocketImpl &&
+                    ((AbstractPlainDatagramSocketImpl)impl).nativeConnectDisabled())) {
                 connectState = ST_CONNECTED_NO_IMPL;
-          }*/
+            } else {
+                try {
+                    getImpl().connect(address, port);
 
-          // socket is now connected by the impl
-          connectState = ST_CONNECTED;
-          getImpl().connect(address, port);
+                    // socket is now connected by the impl
+                    connectState = ST_CONNECTED;
 
-          // Do we need to filter some packets?
-          int avail = getImpl().dataAvailable();
-          if (avail == -1) {
-              throw new SocketException();
-          }
-          explicitFilter = avail > 0;
-          if (explicitFilter) {
-              bytesLeftToFilter = getReceiveBufferSize();
-          }
-          // ----- END android -----
+                    // Do we need to filter some packets?
+                    int avail = getImpl().dataAvailable();
+                    if (avail == -1) {
+                        throw new SocketException();
+                    }
+                    explicitFilter = avail > 0;
+                    if (explicitFilter) {
+                        bytesLeftToFilter = getReceiveBufferSize();
+                    }
+                } catch (SocketException se) {
+                    // connection will be emulated by DatagramSocket
+                    connectState = ST_CONNECTED_NO_IMPL;
+                    // Android-changed: Propagate the SocketException so connect() can store it.
+                    throw se;
+                }
+           }
+        } finally {
+            connectedAddress = address;
+            connectedPort = port;
         }
-
-        /* ----- BEGIN android -----
-        connectedAddress = address;
-        connectedPort = port;
-        ----- END android ----- */
     }
 
 