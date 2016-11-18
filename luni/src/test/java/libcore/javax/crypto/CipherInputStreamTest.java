LineageOS/android_libcore

commit 81690afd3a3da9b8dd22e74d892befb9a69dfe6f

Cut down on object allocation in CipherInputStream
While installing large encrypted app, GC used to collect large
number of objects because when the data was being read from the input
stream, it used to place the same on a new byte array leaving the last
allocated array unused.

The same behavior was not observed on Android N maybe due to changes in
GC, however, it was creating the objects in a similar way it used to
create  in older versions.

This increases speed of CipherInputStreamBenchmark from 54ms to 28 ms.

Test: libcore.javax.crypto.CipherInputStreamTest
Test: org.apache.harmony.crypto.tests.javax.crypto.CipherInputStreamTest
Test: org.apache.harmony.crypto.tests.javax.crypto.CipherInputStream1Test

Bug: 6478569
Change-Id: I0cf0dc37e6513c08abcf2f3077db3dc9e6395b62

ojluni/src/main/java/javax/crypto/CipherInputStream.java
@@ -110,34 +110,43 @@ public class CipherInputStream extends FilterInputStream {
      * returning decrypted data or exceptions.
      */
     private int getMoreData() throws IOException {
+        // Android-changed: The method was creating a new object every time update(byte[], int, int)
+        // or doFinal() was called resulting in the old object being GCed. With do(byte[], int) and
+        // update(byte[], int, int, byte[], int), we use already initialized obuffer.
         if (done) return -1;
+        ofinish = 0;
+        ostart = 0;
+        if (obuffer == null) {
+            obuffer = new byte[cipher.getOutputSize(ibuffer.length)];
+        }
         int readin = input.read(ibuffer);
         if (readin == -1) {
             done = true;
             try {
-                obuffer = cipher.doFinal();
+                // doFinal resets the cipher and it is the final call that is made. If there isn't
+                // any more byte available, it returns 0. In case of any exception is raised, the
+                // cipher gets reset and therefore, it is equivalent to no bytes returned.
+                ofinish = cipher.doFinal(obuffer, 0);
             } catch (IllegalBlockSizeException | BadPaddingException e) {
                 obuffer = null;
                 throw new IOException(e);
+            } catch (ShortBufferException e) {
+                obuffer = null;
+                throw new IllegalStateException("ShortBufferException is not expected", e);
             }
-            if (obuffer == null)
-                return -1;
-            else {
-                ostart = 0;
-                ofinish = obuffer.length;
-                return ofinish;
+        } else {
+            // update returns number of bytes stored in obuffer.
+            try {
+                ofinish = cipher.update(ibuffer, 0, readin, obuffer, 0);
+            } catch (IllegalStateException e) {
+                obuffer = null;
+                throw e;
+            } catch (ShortBufferException e) {
+                // Should not reset the value of ofinish as the cipher is still not invalidated.
+                obuffer = null;
+                throw new IllegalStateException("ShortBufferException is not expected", e);
             }
         }
-        try {
-            obuffer = cipher.update(ibuffer, 0, readin);
-        } catch (IllegalStateException e) {
-            obuffer = null;
-            throw e;
-        }
-        ostart = 0;
-        if (obuffer == null)
-            ofinish = 0;
-        else ofinish = obuffer.length;
         return ofinish;
     }
 

MotorolaMobilityLLC/libcore

commit 26a0f8013a29c4f5d1cad34ae8fb1204e41e4c2d

Test CipherInputStream with growing Cipher.getOutputSize()
New test case for handling Cipher.getOutputStream increasing
due to "unprocessed (buffered) data from a previous update
call, padding, and AEAD tagging".

This test for the bug introduced in commit
81690afd3a3da9b8dd22e74d892befb9a69dfe6f: Cut down on object
allocation in CipherInputStream

Test: CipherInputStreamTest in vogar
Bug: 32643789

Change-Id: I78fe5ee5e195d2c7551955f0c1dc8df6bc7e8317

luni/src/test/java/libcore/javax/crypto/CipherInputStreamTest.java
@@ -21,17 +21,24 @@ import java.io.ByteArrayOutputStream;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.Provider;
+import java.security.Security;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
+import javax.crypto.CipherSpi;
 import javax.crypto.SecretKey;
+import javax.crypto.ShortBufferException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import junit.framework.TestCase;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
 
 public final class CipherInputStreamTest extends TestCase {
@@ -218,5 +225,66 @@ public final class CipherInputStreamTest extends TestCase {
         cis.close();
 
         verify(mockIs, times(1)).close();
+    }
+
+    /**
+     * CipherSpi that increments it's engineGetOutputSize output when
+     * engineUpdate is called.
+     */
+    public static class CipherSpiWithGrowingOutputSize extends MockCipherSpi {
+        private int outputSizeDelta = 0;
+
+        @Override
+        protected int engineGetOutputSize(int inputLen) {
+            return inputLen + outputSizeDelta;
+        }
+
+        @Override
+        protected int engineUpdate(byte[] input, int inputOffset, int inputLen, byte[] output,
+                int outputOffset) throws ShortBufferException {
+            int expectedOutputSize = inputLen + outputSizeDelta++;
+            if ((output.length - outputOffset) < expectedOutputSize) {
+                throw new ShortBufferException();
+            }
+            return expectedOutputSize;
+        }
+
+        @Override
+        protected byte[] engineUpdate(byte[] input, int inputOffset, int inputLen) {
+            int expectedOutputSize = inputLen + outputSizeDelta++;
+            return new byte[expectedOutputSize];
+        }
+
+        @Override
+        protected byte[] engineDoFinal(byte[] input, int inputOffset, int inputLen) {
+            return input;
+        }
+    }
+
+    private static class MockProvider extends Provider {
+        public MockProvider() {
+            super("MockProvider", 1.0, "Mock provider used for testing");
+            put("Cipher.GrowingOutputSize",
+                CipherSpiWithGrowingOutputSize.class.getName());
+        }
+    }
+
+    // http://b/32643789, check that CipherSpi.engineGetOutputSize is called and applied
+    // to output buffer size before calling CipherSpi.egineUpdate(byte[],int,int,byte[],int).
+    public void testCipherOutputSizeChange() throws Exception {
+        Provider mockProvider = new MockProvider();
+
+        Cipher cipher = Cipher.getInstance("GrowingOutputSize", mockProvider);
+
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        InputStream mockEncryptedInputStream = new ByteArrayInputStream(new byte[1024]);
+        try (InputStream is = new CipherInputStream(mockEncryptedInputStream, cipher)) {
+            byte[] buffer = new byte[1024];
+            // engineGetOutputSize returns 512+0, engineUpdate expects buf >= 512
+            assertEquals(512, is.read(buffer));
+            // engineGetOutputSize returns 512+1, engineUpdate expects buf >= 513
+            // and will throw ShortBufferException buffer is smaller.
+            assertEquals(513, is.read(buffer));
+        }
     }
 }