LineageOS/android_libcore

commit 15ab5a903e26caade17bc69468717e8e56f51f23

Adjust cookie max-age from expires time calculation.
Use whenCreated field (set when a cookie is created) for the
max-age calculation. Previous, parsing-related change switched
(erroneously) to use the current time inside the expires
assignor method.

Special case was added to avoid situation where
expires-currentTime == -1 which means an undefined expiry time.
This would create a  persisting cookie (where value of -2 and
lower would result in cookie deletion).

Added test for Android N HttpCookiehasExpired() behavior change.

Test: CtsLibcoreTestCases
Bug: 31039416
Change-Id: Idc5417d16a49d56116ed4a6bef7cf56ee922047f

ojluni/src/main/java/java/net/HttpCookie.java
@@ -108,7 +108,8 @@ public final class HttpCookie implements Cloneable {
     public final String header;
 
     //
-    // Hold the creation time (in seconds) of the http cookie for later
+    // Android-changed: Fixed units, s/seconds/milliseconds/, in comment below.
+    // Hold the creation time (in milliseconds) of the http cookie for later
     // expiration calculation
     private final long whenCreated;
 
@@ -1006,12 +1007,18 @@ public final class HttpCookie implements Cloneable {
                         // Android-changed: Use HttpDate for date parsing,
                         // it accepts broader set of date formats.
                         // cookie.setMaxAge(cookie.expiryDate2DeltaSeconds(attrValue));
+                        // Android-changed: Altered max age calculation to avoid setting
+                        // it to MAX_AGE_UNSPECIFIED (-1) if "expires" is one second in past.
                         Date date = HttpDate.parse(attrValue);
+                        long maxAgeInSeconds = 0;
                         if (date != null) {
-                            cookie.setMaxAge((date.getTime() - System.currentTimeMillis()) / 1000);
-                        } else {
-                            cookie.setMaxAge(0);
+                            maxAgeInSeconds = (date.getTime() - cookie.whenCreated) / 1000;
+                            // If "expires" is in the past, remove the cookie.
+                            if (maxAgeInSeconds < 0) {
+                                maxAgeInSeconds = 0;
+                            }
                         }
+                        cookie.setMaxAge(maxAgeInSeconds);
                     }
                 }
             });

harmony-tests/src/test/java/org/apache/harmony/tests/java/net/HttpCookieTest.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -947,6 +947,19 @@ public class HttpCookieTest extends TestCase {
                 .parse("Set-Cookie:name=test;comment=mycomment;commentURL=url;discard;domain=a.b.com;path=temp;port=79;secure");
         cookie = list.get(0);
         assertEquals(0, cookie.getVersion());
+    }
+
+    // http://b/31039416. Android N+ checks current time in hasExpired.
+    // Repeated invocations of cookie.hasExpired() may return different results
+    // due to time passage.
+    // This was not the case in earlier android versions, where hasExpired
+    // was testing the value of max-age/expires at the time of cookie creation.
+    public void test_hasExpired_checksTime() throws Exception {
+        List<HttpCookie> list = HttpCookie.parse("Set-Cookie:name=test;Max-Age=1");
+        HttpCookie cookie = list.get(0);
+        assertFalse(cookie.hasExpired());
+        Thread.sleep(2000);
+        assertTrue(cookie.hasExpired());
     }
 
     /**

MotorolaMobilityLLC/libcore

commit 2597c23519ef80ad7f5307f739ff5c7019692bbe

Fix bug in expires time calculation
Commit 15ab5a903e26caade17bc69468717e8e56f51f23 introduced
test failures in HttpCookiesTest caused by code that tried
to avoid setting max-age to -1. Instead of setting all negative
values of max-age to 0 we do it only for -1, which has
special meaning (an undefined expires value).

Test: CtsLibcoreTestCases
Bug: 31039416
Change-Id: I2571e7205f727f9085564b41761ff243eb4dff3b

ojluni/src/main/java/java/net/HttpCookie.java
@@ -1013,8 +1013,8 @@ public final class HttpCookie implements Cloneable {
                         long maxAgeInSeconds = 0;
                         if (date != null) {
                             maxAgeInSeconds = (date.getTime() - cookie.whenCreated) / 1000;
-                            // If "expires" is in the past, remove the cookie.
-                            if (maxAgeInSeconds < 0) {
+                            // Avoid MAX_AGE_UNSPECIFIED
+                            if (maxAgeInSeconds == MAX_AGE_UNSPECIFIED) {
                                 maxAgeInSeconds = 0;
                             }
                         }