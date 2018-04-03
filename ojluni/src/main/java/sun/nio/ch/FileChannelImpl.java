CyanogenMod/android_libcore

commit 0f7f0c999e751ab883a247e1409dcc8730c87b9f

Fixes for libcore.java.nio.BufferTest
- Added missing MappedByteBuffer native methods
- Fixed RandomAccessFile#getChannel permissions
- Disabled ByteBuffer#set/isAccessible parts of
BufferTest
- Marked set/isAccessible for removal (it will
be reimplemented in the framework).

Change-Id: I7d1999b35600ff45dd448a8b3671d9373559c1aa

ojluni/src/main/java/sun/nio/ch/FileChannelImpl.java
@@ -37,6 +37,11 @@ import sun.misc.Cleaner;
 import sun.misc.IoTrace;
 import sun.security.action.GetPropertyAction;
 
+// ----- BEGIN android -----
+import android.system.ErrnoException;
+import libcore.io.Libcore;
+// ----- END android -----
+
 public class FileChannelImpl
     extends FileChannel
 {
@@ -848,13 +853,33 @@ public class FileChannelImpl
             if (!isOpen())
                 return null;
             if (size() < position + size) { // Extend file size
+                /* ----- BEGIN android -----
                 if (!writable) {
                     throw new IOException("Channel not open for writing " +
                         "- cannot extend file to required size");
                 }
-                int rv;
+                ----- END android ----- */
+                int rv = 0;
                 do {
-                    rv = nd.truncate(fd, position + size);
+                    // ----- BEGIN android -----
+                    //int rv = nd.truncate(fd, position + size);
+                    try {
+                        rv = nd.truncate(fd, position + size);
+                    } catch (IOException r) {
+                        try {
+                            // If we're dealing with non-regular files, for example,
+                            // character devices such as /dev/zero. In those
+                            // cases, we ignore the failed truncation and continue
+                            // on.
+                            if (android.system.OsConstants.S_ISREG(Libcore.os.fstat(fd).st_mode)) {
+                                throw r;
+                            }
+                        } catch (ErrnoException e) {
+                            e.rethrowAsIOException();
+                        }
+                        break;
+                    }
+                    // ----- END android -----
                 } while ((rv == IOStatus.INTERRUPTED) && isOpen());
             }
             if (size == 0) {

luni/src/test/java/libcore/java/nio/BufferTest.java
@@ -619,7 +619,7 @@ public class BufferTest extends TestCase {
     // http://b/16449607
     public void testDirectByteBufferAlignment() throws Exception {
         ByteBuffer b = ByteBuffer.allocateDirect(10);
-        Field addressField = Buffer.class.getDeclaredField("effectiveDirectAddress");
+        Field addressField = Buffer.class.getDeclaredField("address");
         assertTrue(addressField != null);
         addressField.setAccessible(true);
         long address = addressField.getLong(b);
@@ -889,7 +889,7 @@ public class BufferTest extends TestCase {
         ByteBuffer b2 = b1.duplicate();
         NioUtils.freeDirectBuffer(b1);
         for (ByteBuffer b: new ByteBuffer[] { b1, b2 }) {
-            assertFalse(b.isAccessible());
+          //assertFalse(b.isAccessible());
             try {
                 b.compact();
                 fail();
@@ -907,6 +907,7 @@ public class BufferTest extends TestCase {
         }
     }
 
+    /* setAccessible is not available in OpenJdk's buffers.
     public void testAccess() {
         ByteBuffer b1 = ByteBuffer.allocate(1);
         ByteBuffer b2 = b1.duplicate();
@@ -990,7 +991,7 @@ public class BufferTest extends TestCase {
             testAsMethods(b);
             testGetMethods(b);
         }
-    }
+        }*/
 
     private void testPutMethods(ByteBuffer b) {
         b.position(0);

ojluni/src/main/java/java/io/RandomAccessFile.java
@@ -30,7 +30,6 @@ import sun.nio.ch.FileChannelImpl;
 import sun.misc.IoTrace;
 import android.system.ErrnoException;
 import dalvik.system.CloseGuard;
-import java.nio.NioUtils;
 import libcore.io.IoBridge;
 import libcore.io.Libcore;
 import static android.system.OsConstants.*;
@@ -289,7 +288,17 @@ public class RandomAccessFile implements DataOutput, DataInput, Closeable {
     public final FileChannel getChannel() {
         synchronized (this) {
             if (channel == null) {
-                channel = NioUtils.newFileChannel(this, fd, mode);
+                channel = FileChannelImpl.open(fd, path, true, rw, this);
+                /*
+                 * FileDescriptor could be shared by FileInputStream or
+                 * FileOutputStream.
+                 * Ensure that FD is GC'ed only when all the streams/channels
+                 * are done using it.
+                 * Increment fd's use count. Invoking the channel's close()
+                 * method will result in decrementing the use count set for
+                 * the channel.
+                 */
+                // fd.incrementAndGetUseCount();
             }
             return channel;
         }

ojluni/src/main/java/java/nio/ByteBuffer.java
@@ -2035,6 +2035,8 @@ public abstract class ByteBuffer
     public abstract DoubleBuffer asDoubleBuffer();
 
     // ----- BEGIN android -----
+    // TODO(pszczepaniak): Remove these after adding this functionality
+    // in the framework
     /**
      * @hide
      */
@@ -2048,5 +2050,4 @@ public abstract class ByteBuffer
         throw new UnsupportedOperationException();
     }
     // ----- END android -----
-
 }

ojluni/src/main/java/java/nio/DirectByteBuffer.java
@@ -105,9 +105,6 @@ class DirectByteBuffer
             unsafe.freeMemory(address);
             address = 0;
             Bits.unreserveMemory(size, capacity);
-            // ----- BEGIN android -----
-            DirectByteBuffer.this.freed = true;
-            // ----- END android -----
         }
 
     }
@@ -1035,6 +1032,8 @@ class DirectByteBuffer
     }
 
     // ----- BEGIN android -----
+    // TODO(pszczepaniak): Remove these after adding this functionality
+    // in the framework
     private boolean accessible = true;
     private boolean freed = false;
     /**
@@ -1050,5 +1049,4 @@ class DirectByteBuffer
         return accessible && !freed;
     }
     // ----- END android -----
-
 }

ojluni/src/main/native/MappedByteBuffer.c
@@ -1,0 +1,109 @@
+/*
+ * Copyright (c) 2001, 2010, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+#include "jni.h"
+#include "jni_util.h"
+#include "jvm.h"
+#include "jlong.h"
+#include "java_nio_MappedByteBuffer.h"
+#include <sys/mman.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "JNIHelp.h"
+
+#define NATIVE_METHOD(className, functionName, signature) \
+{ #functionName, signature, (void*)(Java_java_nio_ ## className ## _ ## functionName) }
+
+JNIEXPORT jboolean JNICALL
+Java_java_nio_MappedByteBuffer_isLoaded0(JNIEnv *env, jobject obj, jlong address,
+                                         jlong len, jint numPages)
+{
+    jboolean loaded = JNI_TRUE;
+    int result = 0;
+    int i = 0;
+    void *a = (void *) jlong_to_ptr(address);
+#ifdef __linux__
+    unsigned char *vec = (unsigned char *)malloc(numPages * sizeof(char));
+#else
+    char *vec = (char *)malloc(numPages * sizeof(char));
+#endif
+
+    if (vec == NULL) {
+        JNU_ThrowOutOfMemoryError(env, NULL);
+        return JNI_FALSE;
+    }
+
+    result = mincore(a, (size_t)len, vec);
+    if (result == -1) {
+        JNU_ThrowIOExceptionWithLastError(env, "mincore failed");
+        free(vec);
+        return JNI_FALSE;
+    }
+
+    for (i=0; i<numPages; i++) {
+        if (vec[i] == 0) {
+            loaded = JNI_FALSE;
+            break;
+        }
+    }
+    free(vec);
+    return loaded;
+}
+
+
+JNIEXPORT void JNICALL
+Java_java_nio_MappedByteBuffer_load0(JNIEnv *env, jobject obj, jlong address,
+                                     jlong len)
+{
+    char *a = (char *)jlong_to_ptr(address);
+    int result = madvise((caddr_t)a, (size_t)len, MADV_WILLNEED);
+    if (result == -1) {
+        JNU_ThrowIOExceptionWithLastError(env, "madvise failed");
+    }
+}
+
+
+JNIEXPORT void JNICALL
+Java_java_nio_MappedByteBuffer_force0(JNIEnv *env, jobject obj, jobject fdo,
+                                      jlong address, jlong len)
+{
+    void* a = (void *)jlong_to_ptr(address);
+    int result = msync(a, (size_t)len, MS_SYNC);
+    if (result == -1) {
+        JNU_ThrowIOExceptionWithLastError(env, "msync failed");
+    }
+}
+
+
+static JNINativeMethod gMethods[] = {
+  NATIVE_METHOD(MappedByteBuffer, isLoaded0, "(JJI)Z"),
+  NATIVE_METHOD(MappedByteBuffer, load0, "(JJ)V"),
+  NATIVE_METHOD(MappedByteBuffer, force0, "(Ljava/io/FileDescriptor;JJ)V"),
+};
+
+void register_java_nio_MappedByteBuffer(JNIEnv* env) {
+  jniRegisterNativeMethods(env, "java/nio/MappedByteBuffer", gMethods, NELEM(gMethods));
+}

ojluni/src/main/native/Register.cpp
@@ -51,6 +51,7 @@ extern void register_java_net_PlainDatagramSocketImpl(JNIEnv*);
 extern void register_java_net_PlainSocketImpl(JNIEnv*);
 extern void register_java_net_SocketInputStream(JNIEnv*);
 extern void register_java_net_SocketOutputStream(JNIEnv*);
+extern void register_java_nio_MappedByteBuffer(JNIEnv* env);
 extern void register_java_util_zip_Adler32(JNIEnv* env);
 extern void register_java_util_zip_CRC32(JNIEnv*);
 extern void register_java_util_zip_Deflater(JNIEnv*);
@@ -70,6 +71,7 @@ extern void register_sun_nio_ch_ServerSocketChannelImpl(JNIEnv* env);
 extern void register_sun_nio_ch_SocketChannelImpl(JNIEnv* env);
 extern void register_sun_nio_ch_InheritedChannel(JNIEnv* env);
 extern void register_sun_nio_ch_EPollArrayWrapper(JNIEnv* env);
+
 extern jint net_JNI_OnLoad(JavaVM*, void*);
 
 }
@@ -127,6 +129,7 @@ jint JNI_OnLoad(JavaVM* vm, void*) { JNIEnv* env;
     register_sun_nio_ch_Net(env);
     register_sun_nio_ch_DatagramChannelImpl(env);
     register_sun_nio_ch_EPollArrayWrapper(env);
+    register_java_nio_MappedByteBuffer(env);
     net_JNI_OnLoad(vm, NULL);
     return JNI_VERSION_1_6;
 }

ojluni/src/main/native/java_nio_MappedByteBuffer.h
@@ -1,0 +1,37 @@
+/* DO NOT EDIT THIS FILE - it is machine generated */
+#include <jni.h>
+/* Header for class java_nio_MappedByteBuffer */
+
+#ifndef _Included_java_nio_MappedByteBuffer
+#define _Included_java_nio_MappedByteBuffer
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*
+ * Class:     java_nio_MappedByteBuffer
+ * Method:    isLoaded0
+ * Signature: (JJI)Z
+ */
+JNIEXPORT jboolean JNICALL Java_java_nio_MappedByteBuffer_isLoaded0
+  (JNIEnv *, jobject, jlong, jlong, jint);
+
+/*
+ * Class:     java_nio_MappedByteBuffer
+ * Method:    load0
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_java_nio_MappedByteBuffer_load0
+  (JNIEnv *, jobject, jlong, jlong);
+
+/*
+ * Class:     java_nio_MappedByteBuffer
+ * Method:    force0
+ * Signature: (Ljava/io/FileDescriptor;JJ)V
+ */
+JNIEXPORT void JNICALL Java_java_nio_MappedByteBuffer_force0
+  (JNIEnv *, jobject, jobject, jlong, jlong);
+
+#ifdef __cplusplus
+}
+#endif
+#endif

MotorolaMobilityLLC/libcore

commit 17ef8db27bba8f629baf53e7fbf351bd11bef1a2

Partially verify Android patches for sun.nio.ch.FileChannelImpl.
This CL adds and improves some change markers, and also
integrates a bugfix in truncate() that was missed in the
update from OpenJDK 8u60 to 8u121-b13.

- annotate additions for CloseGuard support
- annotate additions for BlockGuard support
  note: From what I can see, position() won't block yet commit
  5873edf853a48362e8ee4e75181f654d0b88b8ef added BlockGuard
  invocations. I've kept the behavior for now since it's already
  present in released versions of Android.
- missing markers "Allocate a DirectByteBuffer directly.".
  From commit fe0e5d3e460de87d24e0224ef10b089289355c47
- tweak marker "Ignore failed truncation for non-regular files."
  Introduced by commit 0f7f0c999e751ab883a247e1409dcc8730c87b9f.
- missing marker "Move clinit code to JNI registration functions."
  From commit f9d2599c4341d07938392d0d3f23a302755d4f93.
- missing removal marker "Unused method getMappedBufferPool()."
  - already removed in initial import
- Integrate missing bugfix for FileChannelImpl.truncate().
  Upstream bug:
    JDK-8137121: (fc) Infinite loop FileChannel.truncate
  was fixed for OpenJDK 8u72 but was missed in Android's
  update from 8u60 to 8u121-b13.
  The buggy code cast the result of nd.truncate() (a long) to int,
  which led to an infinite loop when that typecast overflowed to
  -3, the value of IOStatus.INTERRUPTED.
  Upstream OpenJDK has a testng regression test "LoopingTruncate",
  but this CL does not integrate it.

Bug: 74377400
Test: Treehugger

Change-Id: Idf6343a79fea4d3b2aebfffd892082c9e88c7712

ojluni/src/main/java/sun/nio/ch/FileChannelImpl.java
@@ -105,10 +105,11 @@ public class FileChannelImpl
         this.parent = parent;
         this.path = path;
         this.nd = new FileDispatcherImpl(append);
-        // Android-added: CloseGuard support.
+        // BEGIN Android-added: CloseGuard support.
         if (fd != null && fd.valid()) {
             guard.open("close");
         }
+        // END Android-added: CloseGuard support.
     }
 
     // Used by FileInputStream.getChannel() and RandomAccessFile.getChannel()
@@ -167,6 +168,7 @@ public class FileChannelImpl
 
     }
 
+    // BEGIN Android-added: CloseGuard support.
     protected void finalize() throws Throwable {
         try {
             if (guard != null) {
@@ -177,6 +179,7 @@ public class FileChannelImpl
             super.finalize();
         }
     }
+    // END Android-added: CloseGuard support.
 
     public int read(ByteBuffer dst) throws IOException {
         ensureOpen();
@@ -294,9 +297,13 @@ public class FileChannelImpl
                 ti = threads.add();
                 if (!isOpen())
                     return 0;
+                // BEGIN Android-added: BlockGuard support.
+                // Note: position() itself doesn't seem to block, so this may be overzealous
+                // when position() is not followed by a read/write operation. http://b/77263638
                 if (append) {
                     BlockGuard.getThreadPolicy().onWriteToDisk();
                 }
+                // END Android-added: BlockGuard support.
                 do {
                     // in append-mode then position is advanced to end before writing
                     p = (append) ? nd.size(fd) : position0(fd, -1);
@@ -322,6 +329,9 @@ public class FileChannelImpl
                 ti = threads.add();
                 if (!isOpen())
                     return null;
+                // Android-added: BlockGuard support.
+                // Note: position() itself doesn't seem to block, so this may be overzealous
+                // when position() is not followed by a read/write operation. http://b/77263638
                 BlockGuard.getThreadPolicy().onReadFromDisk();
                 do {
                     p  = position0(fd, newPosition);
@@ -367,6 +377,7 @@ public class FileChannelImpl
             int rv = -1;
             long p = -1;
             int ti = -1;
+            long rp = -1;
             try {
                 begin();
                 ti = threads.add();
@@ -402,8 +413,8 @@ public class FileChannelImpl
                 if (p > newSize)
                     p = newSize;
                 do {
-                    rv = (int)position0(fd, p);
-                } while ((rv == IOStatus.INTERRUPTED) && isOpen());
+                    rp = position0(fd, p);
+                } while ((rp == IOStatus.INTERRUPTED) && isOpen());
                 return this;
             } finally {
                 threads.remove(ti);
@@ -462,6 +473,7 @@ public class FileChannelImpl
             ti = threads.add();
             if (!isOpen())
                 return -1;
+            // Android-added: BlockGuard support.
             BlockGuard.getThreadPolicy().onWriteToDisk();
             do {
                 n = transferTo0(fd, position, icount, targetFD);
@@ -936,26 +948,25 @@ public class FileChannelImpl
                 return null;
 
             if (filesize < position + size) { // Extend file size
-                // BEGIN Android-changed
+                // BEGIN Android-changed: Unexplained, needs investigation. http://b/77513071
                 /*
                 if (!writable) {
                     throw new IOException("Channel not open for writing " +
                         "- cannot extend file to required size");
                 }
                 */
-                // END Android-changed
-                int rv = 0;
+                // END Android-changed: Unexplained, needs investigation. http://b/77513071
+                int rv;
                 do {
-                    // BEGIN Android-changed
-                    //int rv = nd.truncate(fd, position + size);
+                    // BEGIN Android-changed: Ignore failed truncation for non-regular files.
+                    // For character devices such as /dev/zero, it's expected that truncation
+                    // will fail. For all non-regular files, we thus ignore the failed truncation
+                    // and continue on.
+                    // rv = nd.truncate(fd, position + size);
                     try {
                         rv = nd.truncate(fd, position + size);
                     } catch (IOException r) {
                         try {
-                            // If we're dealing with non-regular files, for example,
-                            // character devices such as /dev/zero. In those
-                            // cases, we ignore the failed truncation and continue
-                            // on.
                             if (android.system.OsConstants.S_ISREG(Libcore.os.fstat(fd).st_mode)) {
                                 throw r;
                             }
@@ -964,7 +975,7 @@ public class FileChannelImpl
                         }
                         break;
                     }
-                    // END Android-changed
+                    // END Android-changed: Ignore failed truncation for non-regular files.
                 } while ((rv == IOStatus.INTERRUPTED) && isOpen());
                 if (!isOpen())
                     return null;
@@ -973,6 +984,13 @@ public class FileChannelImpl
                 addr = 0;
                 // a valid file descriptor is not required
                 FileDescriptor dummy = new FileDescriptor();
+                // Android-changed: Allocate a DirectByteBuffer directly.
+                /*
+                if ((!writable) || (imode == MAP_RO))
+                    return Util.newMappedByteBufferR(0, 0, dummy, null);
+                else
+                    return Util.newMappedByteBuffer(0, 0, dummy, null);
+                */
                 return new DirectByteBuffer(0, 0, dummy, null,
                         (!writable) || (imode == MAP_RO) /* readOnly */);
             }
@@ -981,8 +999,9 @@ public class FileChannelImpl
             long mapPosition = position - pagePosition;
             long mapSize = size + pagePosition;
             try {
-                // If no exception was thrown from map0, the address is valid
+                // Android-added: BlockGuard support.
                 BlockGuard.getThreadPolicy().onReadFromDisk();
+                // If no exception was thrown from map0, the address is valid
                 addr = map0(imode, mapPosition, mapSize);
             } catch (OutOfMemoryError x) {
                 // An OutOfMemoryError may indicate that we've exhausted memory
@@ -1015,6 +1034,20 @@ public class FileChannelImpl
             assert (addr % allocationGranularity == 0);
             int isize = (int)size;
             Unmapper um = new Unmapper(addr, mapSize, isize, mfd);
+            // Android-changed: Allocate a DirectByteBuffer directly.
+            /*
+            if ((!writable) || (imode == MAP_RO)) {
+                return Util.newMappedByteBufferR(isize,
+                                                 addr + pagePosition,
+                                                 mfd,
+                                                 um);
+            } else {
+                return Util.newMappedByteBuffer(isize,
+                                                addr + pagePosition,
+                                                mfd,
+                                                um);
+            }
+            */
             return new DirectByteBuffer(isize, addr + pagePosition, mfd, um,
                     (!writable) || (imode == MAP_RO));
         } finally {
@@ -1023,7 +1056,37 @@ public class FileChannelImpl
         }
     }
 
+    // Android-removed: Unused method getMappedBufferPool().
+    /*
+    /**
+     * Invoked by sun.management.ManagementFactoryHelper to create the management
+     * interface for mapped buffers.
+     *
+    public static sun.misc.JavaNioAccess.BufferPool getMappedBufferPool() {
+        return new sun.misc.JavaNioAccess.BufferPool() {
+            @Override
+            public String getName() {
+                return "mapped";
+            }
+            @Override
+            public long getCount() {
+                return Unmapper.count;
+            }
+            @Override
+            public long getTotalCapacity() {
+                return Unmapper.totalCapacity;
+            }
+            @Override
+            public long getMemoryUsed() {
+                return Unmapper.totalSize;
+            }
+        };
+    }
+    */
+
     // -- Locks --
+
+
 
     // keeps track of locks on this file
     private volatile FileLockTable fileLockTable;
@@ -1245,6 +1308,8 @@ public class FileChannelImpl
     private static native long initIDs();
 
     static {
+        // Android-removed: Move clinit code to JNI registration functions.
+        // IOUtil.load();
         allocationGranularity = initIDs();
     }
 