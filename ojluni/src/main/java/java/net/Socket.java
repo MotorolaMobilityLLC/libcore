b4d236e80ea2cbc966e98351fce11e9b25bca646
Ryan Yu <ryan.yu@mediatek.com>
2018-04-18 14:06:18 +0800

    [ALPS03540179] Add debug logs
    Add debug information.
    
    Change-Id: I90daf05e0da9ebb1f418b5d4ef70bf5d336846e7
    CR-Id: ALPS03540179
    Feature: [Module]Common Data Service Stack

--- ojluni/src/main/java/java/net/Socket.java
+++ ojluni/src/main/java/java/net/Socket.java
@@ -627,6 +627,13 @@ class Socket implements java.io.Closeable {
         } else
             throw new UnsupportedOperationException("SocketImpl.connect(addr, timeout)");
         connected = true;
+        /// M: debug logging
+        if (DebugUtils.isDebugLogOn()) {
+            InetSocketAddress isa = libcore.io.IoBridge.getLocalInetSocketAddress(impl.fd);
+            System.out.println("[socket][" + isa.getAddress() + ":"
+                    + getLocalPort() + "] connected");
+        }
+
         /*
          * If the socket was not bound before the connect, it is now because
          * the kernel will have picked an ephemeral port & a local address
@@ -1535,8 +1542,15 @@ class Socket implements java.io.Closeable {
         synchronized(closeLock) {
             if (isClosed())
                 return;
-            if (created)
+            if (created) {
+                /// M: debug logging
+                if (DebugUtils.isDebugLogOn()) {
+                    InetSocketAddress isa = libcore.io.IoBridge.getLocalInetSocketAddress(impl.fd);
+                    System.out.println(
+                            "close [socket][" + isa.getAddress() + ":" + getLocalPort() + "]");
+                }
                 impl.close();
+            }
             closed = true;
         }
     }
